8af8d9d63c118328a705ccad07d08a74abf23436
Mitcheew <wowmormon@gmail.com>
2019-06-17 12:44:34 -0600

    updated links in links.js

--- src/components/links.js
+++ src/components/links.js
@@ -3,9 +3,9 @@ import React, { Component } from "react"
 export default class Links extends Component{
     render() {
         return (
-                <div>
-                    <a href="">LinkedIn</a>
-                    <a href="">GitHub</a>
+                <div className="links">
+                    <a href="https://www.linkedin.com/in/ethanwmitchell/">LinkedIn</a>
+                    <a href="https://github.com/Mitcheew">GitHub</a>
                 </div>
         )
     }
