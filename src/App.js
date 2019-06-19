09204eeaedaf4e50308d39cf10cee838d679377b
Mitcheew <wowmormon@gmail.com>
2019-06-19 13:22:02 -0600

    imported and added components to main app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Skills from "./components/skills"
+import NameTopBar from "./components/nameTopBar"
+import HeaderImage from "./components/headerImage"
+import ProjectsCarousel from "./components/projectsCarousel"
+import Contact from "./components/contact"
+import Links from "./components/links"
 
 function App() {
   return (
     <div className="App">
+      <NameTopBar />
+      <HeaderImage />
       <Skills />
+      <ProjectsCarousel />
+      <Contact />
+      <Links />
     </div>
   );
 }
