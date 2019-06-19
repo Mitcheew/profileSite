c4e2a39bd497820931bf88162a9a5b01a8fdf4e8
Mitcheew <wowmormon@gmail.com>
2019-06-18 18:20:20 -0600

    fleshed out some of the skills page

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,26 +1,38 @@
 import React, { Component } from "react"
+import AngularLogo from "../images/angular.png"
+import GraphqlLogo from "../images/graphql.png"
+import JavascriptLogo from "../images/javascript.png"
+import NodeLogo from "../images/node.png"
+import PostgresLogo from "../images/postgres.png"
+import ReactLogo from "../images/react.png"
 
 export default class Skills extends Component {
     render() {
         return (
             <div>
                 <span className="skillsList">
-                    <img src="" alt="React" />
+                    <img src={ReactLogo} alt="React" />
+                    <p>React</p>
                 </span>
                 <span className="skillsList">
-                    <img src="" alt="Angular" />
+                    <img src={AngularLogo} alt="Angular" />
+                    <p>Angular</p>
                 </span>
                 <span className="skillsList">
-                    <img src="" alt="Node" />
+                    <img src={NodeLogo} alt="Node" />
+                    <p>Node</p>
                 </span>
                 <span className="skillsList">
-                    <img src="" alt="JavaScript" />
+                    <img src={JavascriptLogo} alt="JavaScript" />
+                    <p>JavaScript</p>
                 </span>
                 <span className="skillsList">
-                    <img src="" alt="GraphQL" />
+                    <img src={GraphqlLogo} alt="GraphQL" />
+                    <p>GraphQL</p>
                 </span>
                 <span className="skillsList">
-                    <img src="" alt="Postgres" />
+                    <img src={PostgresLogo} alt="Postgres" />
+                    <p>Postgres</p>
                 </span>
             </div>
 
