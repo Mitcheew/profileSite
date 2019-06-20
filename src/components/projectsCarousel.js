3bf35750fd01a5442a2739736d762577f2656002
Mitcheew <wowmormon@gmail.com>
2019-06-20 13:21:59 -0600

    added react-slick in preparation for the projects carousel, added .env to gitignore, brought over previously used slider code to projectsCarousel to be modified later for use

--- src/components/projectsCarousel.js
+++ src/components/projectsCarousel.js
@@ -1,11 +1,49 @@
 import React, { Component } from "react"
+import Slider from 'react-slick'
 
-export default class Carousel extends Component{
+export default class Carousel extends Component {
     render() {
-        return (
-                <div>
-                    {/* carousel code here */}
+        let settings = {
+            dots: false,
+            lazyLoad: true,
+            infinite: true,
+            speed: 100,
+            slidesToShow: 1,
+            slidesToScroll: 1
+
+        };
+        let photoReel = this.state.image.map(photo => {
+            return (
+                <div className='center-slide' key={photo.photo_id}>
+                    <img className='post-preview' src={photo.image} alt="" />
                 </div>
+            )
+        })
+        return (
+            <div>
+                <Slider
+                    className='SliderPhotos'
+                    dots={false}
+                    lazyLoad={true}
+                    infinite={true}
+                    speed={100}
+                    slidesToScroll={1}
+                    asNavFor={this.state.nav1}
+                    ref={slider => (this.slider2 = slider)}
+                    slidesToShow={photoReel.length < 5 ?
+                        photoReel.length
+                        :
+                        5
+                    }
+                    swipeToSlide={true}
+                    focusOnSelect={true}
+                >
+                    {photoReel.length > 1 ?
+                        photoReel
+                        :
+                        null
+                    }
+                </Slider>                </div>
         )
     }
 
