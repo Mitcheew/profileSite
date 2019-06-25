2697a5f8fe07f4bec2a1d9f47798949e782b241e
Mitcheew <wowmormon@gmail.com>
2019-06-25 17:09:41 -0600

    set up SASS and slick slider for project carousel

--- src/components/projectsCarousel.js
+++ src/components/projectsCarousel.js
@@ -2,47 +2,73 @@ import React, { Component } from "react"
 import Slider from 'react-slick'
 
 export default class Carousel extends Component {
+    constructor() {
+        super()
+        this.state = {
+            project: [
+                {
+                    project_id: 0,
+                    image: 'https://media.licdn.com/dms/image/C4D03AQFmgEk0L8B-vA/profile-displayphoto-shrink_200_200/0?e=1567036800&v=beta&t=zulFNTd1EXRnjT52OCGxUZeHp4KzLlwGmauIs889Kro',
+                    title: 'Project 1'
+                },
+                {
+                    project_id: 1,
+                    image: 'https://media.licdn.com/dms/image/C4D03AQFmgEk0L8B-vA/profile-displayphoto-shrink_200_200/0?e=1567036800&v=beta&t=zulFNTd1EXRnjT52OCGxUZeHp4KzLlwGmauIs889Kro',
+                    title: 'Project 2'
+                },
+                {
+                    project_id: 2,
+                    image: 'https://media.licdn.com/dms/image/C4D03AQFmgEk0L8B-vA/profile-displayphoto-shrink_200_200/0?e=1567036800&v=beta&t=zulFNTd1EXRnjT52OCGxUZeHp4KzLlwGmauIs889Kro',
+                    title: 'Project 3'
+                },
+                {
+                    project_id: 3,
+                    image: 'https://media.licdn.com/dms/image/C4D03AQFmgEk0L8B-vA/profile-displayphoto-shrink_200_200/0?e=1567036800&v=beta&t=zulFNTd1EXRnjT52OCGxUZeHp4KzLlwGmauIs889Kro',
+                    title: 'Project 4'
+                },
+                {
+                    project_id: 4,
+                    image: 'https://media.licdn.com/dms/image/C4D03AQFmgEk0L8B-vA/profile-displayphoto-shrink_200_200/0?e=1567036800&v=beta&t=zulFNTd1EXRnjT52OCGxUZeHp4KzLlwGmauIs889Kro',
+                    title: 'Project 5'
+                },
+                {
+                    project_id: 5,
+                    image: 'https://media.licdn.com/dms/image/C4D03AQFmgEk0L8B-vA/profile-displayphoto-shrink_200_200/0?e=1567036800&v=beta&t=zulFNTd1EXRnjT52OCGxUZeHp4KzLlwGmauIs889Kro',
+                    title: 'Project 6'
+                }
+            ]
+
+        }
+    }
     render() {
+        let photoReel = this.state.project.map(project => {
+            return (
+                <a href="" className='center-slide' key={project.project_id}>
+                    <img className='post-preview' src={project.image} alt="" />
+                    <p>{project.title}</p>
+                </a>
+            )
+        })
         let settings = {
             dots: false,
             lazyLoad: true,
             infinite: true,
             speed: 100,
-            slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToShow: photoReel.length < 5 ?
+                photoReel.length
+                :
+                5,
+            slidesToScroll: 1,
+            swipeToSlide: true
 
         };
-        let photoReel = this.state.image.map(photo => {
-            return (
-                <div className='center-slide' key={photo.photo_id}>
-                    <img className='post-preview' src={photo.image} alt="" />
-                </div>
-            )
-        })
         return (
             <div>
                 <Slider
                     className='SliderPhotos'
-                    dots={false}
-                    lazyLoad={true}
-                    infinite={true}
-                    speed={100}
-                    slidesToScroll={1}
-                    asNavFor={this.state.nav1}
-                    ref={slider => (this.slider2 = slider)}
-                    slidesToShow={photoReel.length < 5 ?
-                        photoReel.length
-                        :
-                        5
-                    }
-                    swipeToSlide={true}
-                    focusOnSelect={true}
+                    {...settings}
                 >
-                    {photoReel.length > 1 ?
-                        photoReel
-                        :
-                        null
-                    }
+                    {photoReel}
                 </Slider>                </div>
         )
     }
