a7621c91e52b718e916c2f20b263e0ebafffb56f
Mitcheew <wowmormon@gmail.com>
2019-07-09 18:16:00 -0600

    styled the top name bar and prepared each component scss file for future use

--- src/components/nameTopBar.js
+++ src/components/nameTopBar.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react"
 
-export default class NameTopBar extends Component{
+export default class NameTopBar extends Component {
     render() {
         return (
-                <span className="topNameBar">Ethan Mitchell</span>
+            <span className="topNameBar">
+                <p>Ethan Mitchell</p>
+            </span>
         )
     }
 
