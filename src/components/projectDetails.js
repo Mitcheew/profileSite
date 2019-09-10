62bc74ae5e0400cc19ca603843240da6944dc618
Mitcheew <wowmormon@gmail.com>
2019-09-10 15:54:11 -0600

    added my real projects and descriptions with links. nearly ready enough to go live. styled the project details popup as well and sdded an X for people to click.

--- src/components/projectDetails.js
+++ src/components/projectDetails.js
@@ -1,28 +1,31 @@
-import React from "react"
+import React, {useState} from "react"
 
 export default function ProjectDetails(props) {
     // share/star links??
-    const customStyles = {
-        content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            height: '500px', // <-- This sets the height
-            overflow: 'auto' // <-- This tells the modal to scroll
-        }
-    };
+    
+    const [index, setIndex] = useState(props.index)
+    const [numberOfProjects, setNumberOfProjects] = useState(props.numberOfProjects)
+    function nextProject(){
+        numberOfProjects === index + 1? setIndex(0) : setIndex(index + 1)
+    }
+    function previousProject(){
+        index === 0? setIndex(numberOfProjects - 1) : setIndex( index - 1)
+    }
     return (
-        <span style={customStyles.content}>
-            <h2>{props.title[props.index]}</h2>
-            <img src={props.image[props.index]} alt={props.title[props.index]} />
+        <span id="projectDetails">
+            <h2>{props.title[index]}</h2>
+            <span onClick={props.close} className="xOut"></span>
+            {/* <span onClick={() => previousProject()} className="slick-arrow slick-prev previousProject"></span>
+            <span onClick={() => nextProject()} className="slick-arrow slick-next nextProject"></span> */}
+            <img src={props.image[index]} alt={props.title[index]} />
             <h3>About this Project</h3>
-            <p>{props.about[props.index]}</p>
+            <p>{props.about[index]}</p>
             <h3>What I Learned</h3>
-            <p>{props.learn[props.index]}</p>
-            <h3>See this Project and my Code</h3>
-            <a href={props.link[props.index]}>Live Project Site</a>
-            <a href={props.repo[props.index]}>GitHub Repo</a>
+            <p>{props.learn[index]}</p>
+            <span className="projectLinks">
+                <a href={props.link[index]} target="_blank">Project Site</a>
+                <a href={props.repo[index]} target="_blank">GitHub Repo</a>
+            </span>
         </span>
     )
 
--- src/components/projectsCarousel.js
+++ src/components/projectsCarousel.js
@@ -3,61 +3,36 @@ import Slider from 'react-slick'
 import ProjectDetails from "./projectDetails"
 import Media from 'react-media';
 import Popup from 'reactjs-popup'
-import ProPic from '../images/noImage.jpg'
+import ProPic from '../images/profile.PNG'
+import BlogPic from '../images/blog.PNG'
+import FridgePic from '../images/FridgeRaider.PNG'
 
 export default function Carousel() {
     const [project, setProject] = useState([
         {
             project_id: 0,
-            image: ProPic,
-            title: 'Project 1',
-            about: 'About 1',
-            learn: 'Learn 1',
-            link: '#',
-            repo: '#'
+            image: BlogPic,
+            title: 'Private Blog',
+            about: 'This is one of the first few projects I did as I was learning. This is a site where you can create, view and delete posts if you have an admin level account or just view as any other type of user.',
+            learn: 'I learned a lot about how to use postgreSQL when making this site. I also started to learn how to use some CSS and some modules to make the site look better organized. I used HashRouter to help navigate between the different pages.',
+            link: 'https://blog.ethanwmitchell.com/#/',
+            repo: 'https://github.com/Mitcheew/personalPrivateBlog'
         },
         {
             project_id: 1,
-            image: ProPic,
-            title: 'Project 2',
-            about: 'About 2',
-            learn: 'Learn 2',
-            link: '#',
-            repo: '#'
+            image: FridgePic,
+            title: 'Fridge-Raiders',
+            about: 'This was a final group project so that we could graduate from DevMountain. Our group which consisted of Lara Potjewyd, James Stevens, and I decided to create a recipe finding app where you find recipes based on manually entered ingredients',
+            learn: 'I learned how valuable it is to communicate with others on your team who are working on the same project and to divide up responsibilities. My primary job was to create the backend and integrate our chosen API, but I was also able to help my other team members when they got stuck on something and vice-versa. I also learned how valuable time management is and how to prioritize which features you need more than others.',
+            link: 'https://fridge-raiders.site/#/',
+            repo: 'https://github.com/Fridgerators/fridgeraider'
         },
         {
             project_id: 2,
             image: ProPic,
-            title: 'Project 3',
-            about: 'About 3',
-            learn: 'Learn 3',
-            link: '#',
-            repo: '#'
-        },
-        {
-            project_id: 3,
-            image: ProPic,
-            title: 'Project 4',
-            about: 'About 4',
-            learn: 'Learn 4',
-            link: '#',
-            repo: '#'
-        },
-        {
-            project_id: 4,
-            image: ProPic,
-            title: 'Project 5',
-            about: 'About 5',
-            learn: 'Learn 5',
-            link: '#',
-            repo: '#'
-        },
-        {
-            project_id: 5,
-            image: ProPic,
-            title: 'Project 6',
-            about: 'About 6',
-            learn: 'Learn 6',
+            title: 'Profile Site',
+            about: 'This project was created post-graduation so that I could have a profile site created from scratch by me. My brother helped come up with the design and I tweaked it to my liking.',
+            learn: 'While working on this site, I learned about and used react-hooks and I learned more fully how to take advantage of the features of CSS and SASS. The next steps is to learn and implement a GraphQL database so that I can add that to my skillset and be able to put in more projects in the future without having to update a specific page every time I want to add a new project.',
             link: '#',
             repo: '#'
         }
@@ -137,7 +112,7 @@ export default function Carousel() {
         <div className="sliderSection">
             <Popup open={popupTrigger} closeOnDocumentClick onClose={popupClose} modal
             // style={customStyles.content}
-            ><ProjectDetails image={image} title={title} about={about} learn={learn} link={link} repo={repo} index={index} /></Popup>
+            ><ProjectDetails image={image} title={title} about={about} learn={learn} link={link} repo={repo} index={index} close={popupClose} numberOfProjects={project.length} /></Popup>
             <h2>My Projects</h2>
             < Media query='(max-width: 751px)' >
                 {matches => matches ?
